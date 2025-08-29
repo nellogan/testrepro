f9ad72c2f611bfbb0580a60a7d88d9d8ae668cc7
nellogan <nellogan@github.com>
2025-08-29 06:20:36 -0400

    checking if build.gradle has made repro_build_patch.sh necessary

--- android/repro_build_patch.sh
+++ android/repro_build_patch.sh
@@ -1,49 +1,49 @@
 #!/bin/bash
 
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-PROJECT_ROOT="$(cd "${SCRIPT_DIR}/.." && pwd)"
-GRADLE_CACHE_PREFIX="/home/$(whoami)/.gradle/caches/"
-
-## react-native patch
-# Force appmodules.so to omit unnecessary data that can interfere with reproducibility when linking
-CMAKE_FILE="${PROJECT_ROOT}/node_modules/react-native/ReactAndroid/cmake-utils/default-app-setup/CMakeLists.txt"
-echo "set_target_properties(appmodules PROPERTIES LINK_FLAGS \"-Wl,--build-id=none -Wl,--sort-common -Wl,--sort-section=name -Wl,--no-undefined-version -Wl,--no-rosegment -Wl,--strip-all -Wl,--hash-style=gnu\")" >> "$CMAKE_FILE"
-
-CMAKE_FILE="${PROJECT_ROOT}/node_modules/react-native/ReactAndroid/cmake-utils/ReactNative-application.cmake"
-# Compile appmodules.so with -g0 (no debug)
-sed -i '/add_library(${CMAKE_PROJECT_NAME} SHARED ${input_SRC})/a target_compile_options(${CMAKE_PROJECT_NAME} PRIVATE -g0)' "$CMAKE_FILE"
-# Force autolinked libraries to be compiled with -g0 and use appropriate linker flags for reproducibility
-sed -i '/if(EXISTS ${PROJECT_BUILD_DIR}\/generated\/autolinking\/src\/main\/jni\/Android-autolinking.cmake)/,/endif()/c\
-if(EXISTS ${PROJECT_BUILD_DIR}/generated/autolinking/src/main/jni/Android-autolinking.cmake)\
-  include(${PROJECT_BUILD_DIR}/generated/autolinking/src/main/jni/Android-autolinking.cmake)\
-  target_link_libraries(${CMAKE_PROJECT_NAME} ${AUTOLINKED_LIBRARIES})\
-  foreach(autolinked_library ${AUTOLINKED_LIBRARIES})\
-    target_compile_options(${autolinked_library} PRIVATE -g0)\
-    target_link_options(${autolinked_library} PRIVATE -Wl,--build-id=none -Wl,--sort-common -Wl,--sort-section=name -Wl,--no-undefined-version -Wl,--no-rosegment -Wl,--strip-all -Wl,--hash-style=gnu)\
-    target_link_libraries(${autolinked_library} common_flags)\
-  endforeach()\
-endif()' "$CMAKE_FILE"
-# Force codegen files to be compiled with -g0 and use appropriate linker flags for reproducibility
-sed -i '/if(EXISTS ${PROJECT_BUILD_DIR}\/generated\/source\/codegen\/jni\/CMakeLists.txt)/,/endif()/c\
-if(EXISTS ${PROJECT_BUILD_DIR}/generated/source/codegen/jni/CMakeLists.txt)\
-  add_subdirectory(${PROJECT_BUILD_DIR}/generated/source/codegen/jni/ codegen_app_build)\
-  get_property(APP_CODEGEN_TARGET DIRECTORY ${PROJECT_BUILD_DIR}/generated/source/codegen/jni/ PROPERTY BUILDSYSTEM_TARGETS)\
-  target_link_libraries(${CMAKE_PROJECT_NAME} ${APP_CODEGEN_TARGET})\
-  foreach(codegen_target ${APP_CODEGEN_TARGET})\
-    target_compile_options(${codegen_target} PRIVATE -g0)\
-    target_link_options(${codegen_target} PRIVATE -Wl,--build-id=none -Wl,--sort-common -Wl,--sort-section=name -Wl,--no-undefined-version -Wl,--no-rosegment -Wl,--strip-all -Wl,--hash-style=gnu)\
-    target_link_libraries(${codegen_target} common_flags)\
-  endforeach()\
-  string(REGEX REPLACE "react_codegen_" "" APP_CODEGEN_HEADER "${APP_CODEGEN_TARGET}")\
-  target_compile_options(${CMAKE_PROJECT_NAME}\
-    PRIVATE\
-    -DREACT_NATIVE_APP_CODEGEN_HEADER="${APP_CODEGEN_HEADER}.h"\
-    -DREACT_NATIVE_APP_COMPONENT_DESCRIPTORS_HEADER="react/renderer/components/${APP_CODEGEN_HEADER}/ComponentDescriptors.h"\
-    -DREACT_NATIVE_APP_COMPONENT_REGISTRATION=${APP_CODEGEN_HEADER}_registerComponentDescriptorsFromCodegen\
-    -DREACT_NATIVE_APP_MODULE_PROVIDER=${APP_CODEGEN_HEADER}_ModuleProvider\
-  )\
-endif()' "$CMAKE_FILE"
-
+#SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+#PROJECT_ROOT="$(cd "${SCRIPT_DIR}/.." && pwd)"
+#GRADLE_CACHE_PREFIX="/home/$(whoami)/.gradle/caches/"
+#
+### react-native patch
+## Force appmodules.so to omit unnecessary data that can interfere with reproducibility when linking
+#CMAKE_FILE="${PROJECT_ROOT}/node_modules/react-native/ReactAndroid/cmake-utils/default-app-setup/CMakeLists.txt"
+#echo "set_target_properties(appmodules PROPERTIES LINK_FLAGS \"-Wl,--build-id=none -Wl,--sort-common -Wl,--sort-section=name -Wl,--no-undefined-version -Wl,--no-rosegment -Wl,--strip-all -Wl,--hash-style=gnu\")" >> "$CMAKE_FILE"
+#
+#CMAKE_FILE="${PROJECT_ROOT}/node_modules/react-native/ReactAndroid/cmake-utils/ReactNative-application.cmake"
+## Compile appmodules.so with -g0 (no debug)
+#sed -i '/add_library(${CMAKE_PROJECT_NAME} SHARED ${input_SRC})/a target_compile_options(${CMAKE_PROJECT_NAME} PRIVATE -g0)' "$CMAKE_FILE"
+## Force autolinked libraries to be compiled with -g0 and use appropriate linker flags for reproducibility
+#sed -i '/if(EXISTS ${PROJECT_BUILD_DIR}\/generated\/autolinking\/src\/main\/jni\/Android-autolinking.cmake)/,/endif()/c\
+#if(EXISTS ${PROJECT_BUILD_DIR}/generated/autolinking/src/main/jni/Android-autolinking.cmake)\
+#  include(${PROJECT_BUILD_DIR}/generated/autolinking/src/main/jni/Android-autolinking.cmake)\
+#  target_link_libraries(${CMAKE_PROJECT_NAME} ${AUTOLINKED_LIBRARIES})\
+#  foreach(autolinked_library ${AUTOLINKED_LIBRARIES})\
+#    target_compile_options(${autolinked_library} PRIVATE -g0)\
+#    target_link_options(${autolinked_library} PRIVATE -Wl,--build-id=none -Wl,--sort-common -Wl,--sort-section=name -Wl,--no-undefined-version -Wl,--no-rosegment -Wl,--strip-all -Wl,--hash-style=gnu)\
+#    target_link_libraries(${autolinked_library} common_flags)\
+#  endforeach()\
+#endif()' "$CMAKE_FILE"
+## Force codegen files to be compiled with -g0 and use appropriate linker flags for reproducibility
+#sed -i '/if(EXISTS ${PROJECT_BUILD_DIR}\/generated\/source\/codegen\/jni\/CMakeLists.txt)/,/endif()/c\
+#if(EXISTS ${PROJECT_BUILD_DIR}/generated/source/codegen/jni/CMakeLists.txt)\
+#  add_subdirectory(${PROJECT_BUILD_DIR}/generated/source/codegen/jni/ codegen_app_build)\
+#  get_property(APP_CODEGEN_TARGET DIRECTORY ${PROJECT_BUILD_DIR}/generated/source/codegen/jni/ PROPERTY BUILDSYSTEM_TARGETS)\
+#  target_link_libraries(${CMAKE_PROJECT_NAME} ${APP_CODEGEN_TARGET})\
+#  foreach(codegen_target ${APP_CODEGEN_TARGET})\
+#    target_compile_options(${codegen_target} PRIVATE -g0)\
+#    target_link_options(${codegen_target} PRIVATE -Wl,--build-id=none -Wl,--sort-common -Wl,--sort-section=name -Wl,--no-undefined-version -Wl,--no-rosegment -Wl,--strip-all -Wl,--hash-style=gnu)\
+#    target_link_libraries(${codegen_target} common_flags)\
+#  endforeach()\
+#  string(REGEX REPLACE "react_codegen_" "" APP_CODEGEN_HEADER "${APP_CODEGEN_TARGET}")\
+#  target_compile_options(${CMAKE_PROJECT_NAME}\
+#    PRIVATE\
+#    -DREACT_NATIVE_APP_CODEGEN_HEADER="${APP_CODEGEN_HEADER}.h"\
+#    -DREACT_NATIVE_APP_COMPONENT_DESCRIPTORS_HEADER="react/renderer/components/${APP_CODEGEN_HEADER}/ComponentDescriptors.h"\
+#    -DREACT_NATIVE_APP_COMPONENT_REGISTRATION=${APP_CODEGEN_HEADER}_registerComponentDescriptorsFromCodegen\
+#    -DREACT_NATIVE_APP_MODULE_PROVIDER=${APP_CODEGEN_HEADER}_ModuleProvider\
+#  )\
+#endif()' "$CMAKE_FILE"
+#
 ### react-native-screens patch
 ## Force rnscreens.so to omit unnecessary data that can interfere with reproducibility when linking
 #CMAKE_FILE="${PROJECT_ROOT}/node_modules/react-native-screens/android/CMakeLists.txt"
